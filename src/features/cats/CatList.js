9bc9348d9204744c9c016d53e9cbc0c8e620e487
sebastian medina <sebastianlopez_21@hotmail.com>
2023-07-16 13:12:55 -0400

    commit tree 76bc34fa0f45576976b8b709e851afb6708dd610

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,21 @@
+import React from "react";
+
 // write your CatList component here
+function CatList({catPics}){
+
+
+    return (
+        <div>
+            <ul>
+            {catPics.map((cat)=> (
+                <li key={cat.id}>
+                    <img alt="cat" src={cat.url}/>
+                </li>
+            ))}
+            </ul>
+        </div>
+    ); 
+}
+
+
+export default CatList;
